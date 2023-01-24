e0d16eac41a6f807356bb4c28cf881a02933b162
João Paulo Vieira <contato@joaopaulovieira.dev>
2023-01-24 14:41:22 -0300

    first commit

---
+++ android/app/src/main/kotlin/dev/joaopaulovieira/portfolio2/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.joaopaulovieira.portfolio2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home/components/header.dart
@@ -0,0 +1,181 @@
+// @dart=2.9
+import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:portfolio2/models/header_item.dart';
+import 'package:portfolio2/utils/constants.dart';
+import 'package:portfolio2/utils/globals.dart';
+import 'package:portfolio2/utils/screen_helper.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+
+List<HeaderItem> headerItems = [
+  HeaderItem(
+    title: "HOME",
+    onTap: () {},
+  ),
+  HeaderItem(title: "MY INTRO", onTap: () {}),
+  HeaderItem(title: "SERVICES", onTap: () {}),
+  HeaderItem(title: "PORTFOLIO", onTap: () {}),
+  HeaderItem(title: "TESTIMONIALS", onTap: () {}),
+  HeaderItem(title: "BLOGS", onTap: () {}),
+  HeaderItem(
+    title: "HIRE ME",
+    onTap: () {},
+    isButton: true,
+  ),
+];
+
+class HeaderLogo extends StatelessWidget {
+  const HeaderLogo({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      child: GestureDetector(
+        onTap: () {},
+        child: RichText(
+          text: TextSpan(
+            children: [
+              TextSpan(
+                text: "M",
+                style: GoogleFonts.oswald(
+                  color: Colors.white,
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              TextSpan(
+                text: ".",
+                style: GoogleFonts.oswald(
+                  color: kPrimaryColor,
+                  fontSize: 36.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class HeaderRow extends StatelessWidget {
+  const HeaderRow({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveVisibility(
+      visible: false,
+      visibleWhen: const [
+        Condition.largerThan(name: MOBILE),
+      ],
+      child: Row(
+        children: headerItems
+            .map(
+              (item) => item.isButton
+                  ? MouseRegion(
+                      cursor: SystemMouseCursors.click,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: kDangerColor,
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20.0, vertical: 5.0),
+                        child: TextButton(
+                          onPressed: item.onTap,
+                          child: Text(
+                            item.title,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 13.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  : MouseRegion(
+                      cursor: SystemMouseCursors.click,
+                      child: Container(
+                        margin: const EdgeInsets.only(right: 30.0),
+                        child: GestureDetector(
+                          onTap: item.onTap,
+                          child: Text(
+                            item.title,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 13.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
+
+class Header extends StatelessWidget {
+  const Header({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenHelper(
+      desktop: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: buildHeader(),
+      ),
+      // We will make this in a bit
+      mobile: buildMobileHeader(),
+      tablet: buildHeader(),
+    );
+  }
+
+  // mobile header
+  Widget buildMobileHeader() {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const HeaderLogo(),
+            // Restart server to make icons work
+            // Lets make a scaffold key and create a drawer
+            GestureDetector(
+              onTap: () {
+                // Lets open drawer using global key
+                Globals.scaffoldKey.currentState.openEndDrawer();
+              },
+              child: const Icon(
+                FlutterIcons.menu_fea,
+                color: Colors.white,
+                size: 28.0,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Lets plan for mobile and smaller width screens
+  Widget buildHeader() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: const [
+          HeaderLogo(),
+          HeaderRow(),
+        ],
+      ),
+    );
+  }
+}
